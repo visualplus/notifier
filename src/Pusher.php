73f3be1c310565e33190209d5838c4f86a27623d
programmer <visualplus@naver.com>
2016-01-07 14:44:45 +0900

    수정

--- src/Pusher.php
+++ src/Pusher.php
@@ -40,8 +40,8 @@ class Pusher
         $config = config('pusher');
 
         // 안드로이드 디바이스 아이디가 있으면 푸시 전송
-        foreach ($user->getAndroidDeviceId() as $androidDeviceId) {
-            $this->androidPusher->send($androidDeviceId, $message->getAndroidMessage());
+        if (COUNT($user->getAndroidDeviceId()) > 0) {
+            $this->androidPusher->send($user->getAndroidDeviceId(), $message->getAndroidMessage());
         }
 
         // 핸드폰번호가 세팅되어있으면 문자보내기 큐에다 저장
